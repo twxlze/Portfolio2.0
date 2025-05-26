ee3407d3170a79e2519f053c376adef26c29f0a0
twxlze <lisa.meunier71500@gmail.com>
2025-05-26 21:13:56 +0200

    feat : japonese love

--- parallax.js
+++ parallax.js
@@ -1,11 +1,15 @@
 let lune = document.getElementById('lune');
 let forest = document.getElementById('forest-font');
+let love = document.getElementById('love');
+
 
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
     lune.style.marginTop = value * -0.5 + 'px';
     lune.style.marginLeft = value * -0.5 + 'px';
+    love.style.marginRight = value * -0.01 + 'em';
+
 
     forest.style.marginTop = value * -1 + 'px';
 
