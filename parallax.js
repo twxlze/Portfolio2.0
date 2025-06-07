602240eece683921775879785255c4cbeed9c890
Meunier Lisa <lisa.meunier@iut-dijon.u-bourgogne>
2025-06-07 18:48:03 +0200

    fix: career

--- parallax.js
+++ parallax.js
@@ -1,7 +1,8 @@
 let lune = document.getElementById('lune');
 let forest = document.getElementById('forest-font');
 let love = document.getElementById('love');
-let bat = document.getElementById('batbat');
+let career = document.getElementById('mycareer'); 
+let font2 = document.getElementById('font-2'); 
 
 
 
@@ -10,7 +11,8 @@ window.addEventListener('scroll', () => {
     lune.style.marginTop = value * -0.5 + 'px';
     lune.style.marginLeft = value * -0.5 + 'px';
     love.style.marginRight = value * -0.01 + 'em';
-
-
     forest.style.marginTop = value * -1 + 'px';
+    career.style.marginLeft = value * -1 + 'px';
+    font2.style.marginRight = value * -1 + 'px';
+
 });
